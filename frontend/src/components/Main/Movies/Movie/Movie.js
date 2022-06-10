b9e0868ae8f8c09edaa0c9e1b7c097c8f291db19
bulabd <bulabd@outlook.com>
2022-06-10 16:16:29 -0400

    Give a default poster image if none is present

--- frontend/src/components/Main/Movies/Movie/Movie.js
+++ frontend/src/components/Main/Movies/Movie/Movie.js
@@ -3,7 +3,13 @@ import "./Movie.css"
 export default function Movie(props) {
   return (
     <div>
-      <img src={`https://image.tmdb.org/t/p/w200${props.poster}`}></img>
+      <img 
+        src={`https://image.tmdb.org/t/p/w200${props.poster}`} 
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null;
+          currentTarget.src="http://www.movienewsletters.net/photos/000000H1.jpg";
+        }}>
+      </img>
       {props.title}
       {props.rating}
     </div>
