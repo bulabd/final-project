0614b872cb3a8aee04cb7a0c17e06fbf41735a1e
Baila3 <bailaly16@gmail.com>
2022-06-10 15:14:48 -0400

    style the Footer and nav

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,7 +1,13 @@
 import "./Footer.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer(props) {
   return(
-    <h1>Footer</h1>
+    <div className="footer-div">
+   <a href="https://github.com/bulabd"><FontAwesomeIcon icon={faGithub} /> Bulat Abdullin</a>
+   <a href="https://github.com/George-was-here"><FontAwesomeIcon icon={faGithub} /> George Burt</a>
+   <a href="https://github.com/Baila3"><FontAwesomeIcon icon={faGithub} /> Baila Ly</a>
+    </div>
   );
 }
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,7 +1,15 @@
-import "./Navbar.css"
+import "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFilm} from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar(props) {
   return(
-    <h1>Navbar</h1>
+    <div className="nav-div">
+      <h1>Movies<FontAwesomeIcon icon={faFilm}/> </h1>
+      <div className="user">
+        <a href="/login">Login</a>
+        <a href="/SignUp"> /Register</a>
+      </div>
+    </div>
   );
 }
