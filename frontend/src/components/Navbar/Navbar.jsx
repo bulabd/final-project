9c0bdc29aef989a9d51bccaba3a1d0cbdb97dcd1
bulabd <bulabd@outlook.com>
2022-06-20 18:18:05 -0400

    Fix logo path and backend .env (API key)

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,6 @@ import "./Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faFilm} from '@fortawesome/free-solid-svg-icons';
 
-
 export default function Navbar() {
   const [cookies] = useCookies();
 
@@ -15,13 +14,13 @@ export default function Navbar() {
 
     <div className="nav-div">
       <div className="logo-container">
-      <Link className="NavMage" to="/" title="Phone home"><img src={require('file:///Users/bailaly/Documents/Screen%20Shot%202022-06-18%20at%206.32.11%20PM.png')} alt="" width = "50" height = "auto"/></Link>
+      <Link className="NavMage" to="/" title="Phone home"><img src={require('../../docs/logo.png')} alt="" width = "50" height = "auto"/></Link>
       <h1>Everyone's a Critic <FontAwesomeIcon icon={faFilm}/></h1>
       </div>
       <div className="user">
         {isLoggedIn ? (
           <>
-         <h4 className="welcome-back">Welcome back {cookies.nameCookie}!</h4>
+         <h4 className="welcome-back">Welcome back {cookies.emailCookie}!</h4>
          <button className="Navbut"><Link className="NavLink" to="/playlists" title="Playlists">Playlists</Link></button>
          <button className="Navbut"><Link className="NavLink" to="/user-dashboard" title="Dashboard">Dashboard</Link></button>
          <button className="Navbut"><Link className="NavLink" to="/logout" title="Logout User">Logout</Link></button>
