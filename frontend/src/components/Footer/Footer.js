c2aaf6476faf1cdabb3ec99dae7ff40171a50175
bulabd <bulabd@outlook.com>
2022-06-21 13:22:39 -0400

    Style the footer and navbar

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ import {faGithub} from '@fortawesome/free-brands-svg-icons';
 export default function Footer(props) {
   return(
     <div className="footer-div">
-      <a href="https://github.com/bulabd" target="_blank"><FontAwesomeIcon icon={faGithub} /> Bulat Abdullin</a>
-      <a href="https://github.com/George-was-here" target="_blank"><FontAwesomeIcon icon={faGithub} /> George Burt</a>
-      <a href="https://github.com/Baila3" target="_blank"><FontAwesomeIcon icon={faGithub} /> Baila Ly</a>
+      <a className="footerLink" href="https://github.com/bulabd" target="_blank"><FontAwesomeIcon icon={faGithub} /> Bulat Abdullin</a>
+      <a className="footerLink" href="https://github.com/George-was-here" target="_blank"><FontAwesomeIcon icon={faGithub} /> George Burt</a>
+      <a className="footerLink" href="https://github.com/Baila3" target="_blank"><FontAwesomeIcon icon={faGithub} /> Baila Ly</a>
     </div>
   );
 }
